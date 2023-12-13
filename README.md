66da36b4884b42fac7f79a1579978a22360f4dab
Mr. Pandaman <149182376+MrPandamaaan@users.noreply.github.com>
2023-12-13 16:56:52 +0100

    Added Github icon, minor changes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MrPandaman-Socials-Webpage
- 
+
+## Introduction
+
+This is a beginner-level project to create a hub for all my social media links. As a newbie in web development, I welcome feedback and suggestions for improvement.
+
+## Features
+
+- Social Media Integration: Links to all my social profiles.
+- Responsive Design: Compatible with desktop and mobile devices.
+
+## Feedback
+
+Open to critique! Feel free to open an issue or submit a pull request with your suggestions.
+
+Thank you for your support and feedback!
--- meinScript.js
+++ meinScript.js
@@ -1,19 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.div6').addEventListener('click', function() {
-      window.location.href = 'https://static-cdn.jtvnw.net/jtv_user_pictures/4335ea33-2725-4ae5-b919-b521043f2ad2-profile_image-300x300.png';
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div6").addEventListener("click", function () {
+    window.location.href =
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/4335ea33-2725-4ae5-b919-b521043f2ad2-profile_image-300x300.png";
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.div8').addEventListener('click', function() {
-      window.location.href = 'https://twitter.com/MrPandaman22';
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div7").addEventListener("click", function () {
+    window.location.href = "https://t.me/mrpandaman22";
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.div7').addEventListener('click', function() {
-      window.location.href = 'https://t.me/mrpandaman22';
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div8").addEventListener("click", function () {
+    window.location.href = "https://twitter.com/MrPandaman22";
   });
 });
 
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div9").addEventListener("click", function () {
+    window.location.href = "https://github.com/MrPandamaaan";
+  });
+});

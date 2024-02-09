fddb682e40f9ef2f139b42121f14a61931cde06c
Mr. Pandaman <149182376+MrPandamaaan@users.noreply.github.com>
2024-02-09 17:49:01 +0100

    Added About Me Page and automatic age calculation

--- meinScript.js
+++ meinScript.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".div6").addEventListener("click", function () {
-    window.location.href =
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/4335ea33-2725-4ae5-b919-b521043f2ad2-profile_image-300x300.png";
+  document.querySelector(".div5").addEventListener("click", function () {
+    window.location.href = "https://bsky.app/profile/mrpandaman.bsky.social";
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".div5").addEventListener("click", function () {
-    window.location.href = "https://bsky.app/profile/mrpandaman.bsky.social";
+  document.querySelector(".div6").addEventListener("click", function () {
+    window.location.href =
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/4335ea33-2725-4ae5-b919-b521043f2ad2-profile_image-300x300.png";
   });
 });
 
@@ -29,5 +29,33 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div10").addEventListener("click", function () {
+    window.location.href = "./about.html";
+  });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".div11").addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
+});
 
+function calculateAge(birthDate) {
+  var today = new Date();
+  var birthDate = new Date(birthDate);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+
+function updateAge() {
+  var birthDate = "2001-07-13"; // Ihr Geburtsdatum im Format JJJJ-MM-TT
+  var age = calculateAge(birthDate);
+  document.getElementById("age").textContent = age;
+}
+
+window.onload = updateAge;

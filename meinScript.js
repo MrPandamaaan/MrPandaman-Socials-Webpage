88c4aeab1cfa3ba8f46ee262bb1701db3f7a8467
Mr. Pandaman <149182376+MrPandamaaan@users.noreply.github.com>
2024-07-01 10:29:27 +0200

    Added Art Page

--- meinScript.js
+++ meinScript.js
@@ -6,8 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".div6").addEventListener("click", function () {
-    window.location.href =
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/4335ea33-2725-4ae5-b919-b521043f2ad2-profile_image-300x300.png";
+    window.location.href = "./art.html";
   });
 });
 
